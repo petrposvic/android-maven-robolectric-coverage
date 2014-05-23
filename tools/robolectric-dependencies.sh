b18ee600f162971a2c7a62698dc18c7bdf49c0e0
Petr Posvic <petr@posvic.cz>
2014-05-23 19:43:39 +0200

    Upgrade to Robolectric 2.3

---
+++ tools/robolectric-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mvn install:install-file -DgroupId=com.google.android.maps \
+  -DartifactId=maps \
+  -Dversion=18_r3 \
+  -Dpackaging=jar \
+  -Dfile="$ANDROID_HOME/add-ons/addon-google_apis-google-18/libs/maps.jar"
+
+mvn install:install-file -DgroupId=com.android.support \
+  -DartifactId=support-v4 \
+  -Dversion=19.0.1 \
+  -Dpackaging=jar \
+  -Dfile="$ANDROID_HOME/extras/android/support/v4/android-support-v4.jar"
